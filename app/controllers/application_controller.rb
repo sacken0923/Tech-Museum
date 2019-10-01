8ce628ec610fa611dae5ae53752c764b3cf82afb
sacken0923 <sacken0923@gmail.com>
2019-10-01 12:39:54 +0900

    revise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:singup,keys:[:nickname,:profile,:icon)]
+    devise_parameter_sanitizer.permit(:singup,keys:[:nickname,:profile,:icon])
   end
 end
